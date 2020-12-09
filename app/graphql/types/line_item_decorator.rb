17725a9ac09e9a61f2eeaa9173dcbff7a02354b0
Angel Mendez <angelmendezz751@gmail.com>
2020-12-09 15:41:38 -0600

    Cart demo

--- app/graphql/types/line_item_decorator.rb
+++ app/graphql/types/line_item_decorator.rb
@@ -1,14 +1,23 @@
 module Graphql
   module Types
     module LineItemDecorator
+      include ActionView::Helpers::NumberHelper
       def self.prepended(base)
         base.field :product, SolidusGraphqlApi::Types::Product, null: true
+        base.field :displayPriceAmount, GraphQL::Types::String, null: true
+        base.field :displayAmount, GraphQL::Types::String, null: true
+      end
+
+      def displayPriceAmount
+        ActionController::Base.helpers.number_to_currency(object.price)
+      end
+
+      def displayAmount
+        ActionController::Base.helpers.number_to_currency(object.amount)
       end
 
       def product
-        # SolidusGraphqlApi::BatchLoader.for(Spree::LineItem.last, :product)
         object.product
-        # SolidusGraphqlApi::BatchLoader.for(object.product, :product)
       end
 
       SolidusGraphqlApi::Types::LineItem.prepend self
