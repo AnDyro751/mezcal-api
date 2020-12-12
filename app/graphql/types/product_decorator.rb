2e5f903ad033932a29fd222d4a9a2718462e78f5
Angel Mendez <angelmendezz751@gmail.com>
2020-12-12 14:09:18 -0600

    Add taxons in product query

--- app/graphql/types/product_decorator.rb
+++ app/graphql/types/product_decorator.rb
@@ -4,10 +4,20 @@ module Graphql
     module ProductDecorator
       def self.prepended(base)
         base.field :depth_variants, SolidusGraphqlApi::Types::Variant.connection_type, null: true
+        base.field :taxons, SolidusGraphqlApi::Types::Taxon.connection_type, null: true
+        base.field :optionTypes, SolidusGraphqlApi::Types::OptionType.connection_type, null: true
       end
 
       def depth_variants
         object.variants.includes(option_values: [:option_type])
+      end
+
+      def taxons
+        object.taxons
+      end
+
+      def optionTypes
+        object.option_types.includes(:option_values)
       end
 
       SolidusGraphqlApi::Types::Product.prepend self
