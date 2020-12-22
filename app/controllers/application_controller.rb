905a73d4686fcec203377ad62459b3b17520affd
Angel Mendez <angelmendezz751@gmail.com>
2020-12-22 16:40:12 -0600

    Todo funciona

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
-  before_action :show_session
-
-  def show_session
-    @order = Spree::Order.find_by(guest_token: 'VtbcDEmPyDlh0qpZ4t7mVQ')
-  end
 end
