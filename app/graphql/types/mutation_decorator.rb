59e98e9c3c13a3bdd6917470be7b477f14aaddcb
Angel Mendez <angelmendezz751@gmail.com>
2021-01-05 13:50:49 -0600

    Vamos a actualizar el costo de envio al seleccionar un nuevo estado

--- app/graphql/types/mutation_decorator.rb
+++ app/graphql/types/mutation_decorator.rb
@@ -9,10 +9,44 @@ module Graphql
                    Graphql::Types::StripeCheckoutItem,
                    null: true,
                    description: 'Create stripe checkout'
+        base.field :updateStateOrder,
+                   SolidusGraphqlApi::Types::Order,
+                   null: true,
+                   description: 'Update order state' do
+          argument :state_id, GraphQL::Types::ID, required: true
+        end
+      end
+
+      def updateStateOrder(state_id:)
+        return nil if current_order.nil?
+        state_id = Base64.decode64(state_id).split('-')[-1]
+        puts "----current order is nil" if current_order.nil?
+        state = Spree::State.find_by(id: state_id)
+        puts "----state is nil" if state.nil?
+        return nil if state.nil?
+        bill_address = current_order.bill_address.attributes
+        bill_address.delete("id")
+        update_params = {
+            bill_address: Spree::Address.new(bill_address),
+            ship_address: Spree::Address.new(bill_address)
+        }
+        begin
+          if Spree::OrderUpdateAttributes.new(current_order, update_params).apply
+            current_order.recalculate
+            current_order
+          else
+            return nil
+          end
+
+        rescue => e
+          puts "----------#{e}"
+          nil
+        end
       end
 
       def createCheckout
         return nil if current_order.nil?
+
         Stripe.api_key = 'sk_test_51H9CZeBOcPJ0nbHctTzfQZhFXBnn8j05e0xqJ5RSVz5Bum72LsvmQKIecJnsoHISEg0jUWtKjERYGeCAEWiIAujP00Fae9MiKm'
         begin
           session = Stripe::Checkout::Session
